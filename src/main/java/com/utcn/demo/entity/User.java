bbb0b512e84226b03dd6f470ff53a746368e9cc2
Alin Anghelus <anghelusalin02@gmail.com>
2024-06-04 18:32:20 +0300

    added "moderator" column for user

--- src/main/java/com/utcn/demo/entity/User.java
+++ src/main/java/com/utcn/demo/entity/User.java
@@ -19,6 +19,14 @@ public class User {
     private String password;
     @Column(name = "score")
     private int score;
+    @Column(name = "moderator")
+    private boolean moderator;
+    private boolean getModerator(){
+        return moderator;
+    }
+    private void setModerator(boolean moderator){
+        this.moderator = moderator;
+    }
     public Long getUserId() {
         return userId;
     }
@@ -55,20 +63,22 @@ public class User {
     public void setScore(int score) {
         this.score = score;
     }
-    public User(String username, String email, String phoneNumber, String password, int score) {
+    public User(String username, String email, String phoneNumber, String password, int score, boolean moderator) {
         this.username = username;
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.password = password;
         this.score = score;
+        this.moderator = moderator;
     }
-    public User(Long userId, String username, String email, String phoneNumber, String password, int score) {
+    public User(Long userId, String username, String email, String phoneNumber, String password, int score, boolean moderator) {
         this.userId = userId;
         this.username = username;
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.password = password;
         this.score = score;
+        this.moderator = moderator;
     }
     public User(){}
 }
